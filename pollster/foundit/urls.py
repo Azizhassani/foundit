c9c16130bc9b265231c238dca7f406b530e79ddb
aziz <hassaniaziz2005@gmail.com>
2025-02-25 19:01:13 +0100

    Describe what you changed

--- pollster/foundit/urls.py
+++ pollster/foundit/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import index,add_lost_item,register
+from .views import index,add_lost_item,register,user_login,user_logout
 
 urlpatterns = [
-    path('lost-items/', index, name='lost_items_list'),
+    path('lost-items-list/', index, name='lost_items_list'),
     path('add-lost-item/',add_lost_item, name='add_lost_item'),
     path('register/',register,name='register'),
+    path('login/',user_login,name='login'),
+    path('logout/',user_logout,name='logout'),
 ]
--- pollster/foundit/views.py
+++ pollster/foundit/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,redirect
 from .models import founditem
 from .forms import foundForm ,registerform
-from django.contrib.auth import login
+from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -9,6 +11,7 @@ def index(request):
     items = founditem.objects.all()
     return render(request,'foundit/index.html',{'items':items})
 
+@login_required
 def add_lost_item(request):
     if request.method == "POST":
         form = foundForm(request.POST)
@@ -27,9 +30,27 @@ def register(request):
             user.set_password(form.cleaned_data['password'])  # Hash
             user.save()  
             login(request, user)  
-            return redirect('home')  
+            return redirect('lost_items_list')  
     else:
         form = registerform()
     
     return render(request, 'foundit/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user) 
+            return redirect('lost_items_list')  
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'foundit/login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)  
+    return redirect('lost_items_list')  
+
+
 
